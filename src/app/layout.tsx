0362988181577daee402864d659561bb43c09fa4
Konstantin Lindner <hello@konte.se>
2023-09-23 23:40:58 +0200

    rebuild

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
-export const dynamic = 'force-dynamic';
+// change
 
-import './globals.css';
-import type { Metadata } from 'next';
+export const dynamic = "force-dynamic";
+
+import "./globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Equi Sync',
-  description: '',
+  title: "Equi Sync",
+  description: "",
 };
 
 export default function RootLayout({
@@ -14,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html data-theme='winter' lang='en'>
+    <html data-theme="winter" lang="en">
       <body className="text-neutral font-roboto font-medium">{children}</body>
     </html>
   );
