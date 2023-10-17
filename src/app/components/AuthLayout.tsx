288626f1e3e10e3222bdc3ce29a4cace56e11fe2
Aliciaolofsson <Alicia-olofsson@hotmail.com>
2023-10-17 13:11:19 +0200

    remove svg

--- src/app/components/AuthLayout.tsx
+++ src/app/components/AuthLayout.tsx
@@ -48,20 +48,7 @@ const AuthLayout: React.FC<AuthLayoutProps> = ({
               href='/'
               className='btn bg-transparent border-transparent p-1 '
             >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                stroke-width='2'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                className='lucide-chevron-left'
-              >
-                <path d='m15 18-6-6 6-6' />
-              </svg>
+              Home
             </Link>
           </div>
 
